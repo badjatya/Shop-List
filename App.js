73dde2382d3ac7eea324b2cb9ccc7e73a21b9814
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-29 00:42:38 +0530

    List Item Component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "./components/header.component";
 import InputContainer from "./components/InputContainer.component";
+import ListItem from "./components/listItem.component";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
       <InputContainer />
+      <ListItem />
     </View>
   );
 }
---
+++ components/listItem.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View , Text} from "react-native";
+
+const ListItem = props => (
+    <Text>HII</Text>
+)
+
+const styles = StyleSheet.create({
+    container: {},
+});
+
+export default ListItem;
