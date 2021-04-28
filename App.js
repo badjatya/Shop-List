c9c7f152f69266c0d4aa2338f7dd030769222cd2
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-28 23:46:39 +0530

    Input container

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./components/header.component";
+import InputContainer from "./components/InputContainer.component";
 
 export default function App() {
   return (
