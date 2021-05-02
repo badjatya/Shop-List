3fe3a0ecf7655904463e008be11681d8d8f916f4
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-02 22:35:03 +0530

    List Style

--- components/listItem.component.jsx
+++ components/listItem.component.jsx
@@ -5,14 +5,22 @@ const ListItem = props => (
     <View style={styles.container}>
         {
             props.lists.map(item => (
-                <Text key={Math.random()}>{item}</Text>
+                <TouchableOpacity style={styles.listContainer} key={Math.random()}>
+                    <Text>{item}</Text>
+                </TouchableOpacity>
             ))
         }
     </View>
 )
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        paddingHorizontal:40,
+        marginTop:20,
+    },
+    listContainer:{
+
+    }
 });
 
 export default ListItem;
